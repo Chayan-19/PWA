6d392a3530fc5b9e00598bd87481801b0acb58b0
Chayan <55481820+Chayan-19@users.noreply.github.com>
2020-11-09 16:49:47 +0530

    Update worker.js

--- client/worker.js
+++ client/worker.js
@@ -1,4 +1,6 @@
 console.log("Service Worker Loaded...");
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://localhost:1883");
 
 self.addEventListener("install", e => {
   e.waitUntil(
@@ -12,11 +14,26 @@ self.addEventListener("fetch", e => {
   console.log(`Intercepting fetch request for ${e.request.url}`);
 });
 
-self.addEventListener("push", e => {
-  const data = e.data.json();
-  console.log("Push Recieved...");
-  self.registration.showNotification(data.title, {
-    body: "Notified!",
-    icon: "http://image.ibb.co/frYOFd/tmlogo.png",
+client.on("message", function (topic, message) {
+  // message is Buffer
+  self.addEventListener("push", e => {
+    const data = e.data.json();
+    console.log("Push Recieved...");
+    self.registration.showNotification(data.title, {
+      body: "Notified!",
+      icon: "http://image.ibb.co/frYOFd/tmlogo.png",
+    });
   });
+
+  console.log(message.toString());
+  client.end();
 });
+
+// self.addEventListener("push", e => {
+//   const data = e.data.json();
+//   console.log("Push Recieved...");
+//   self.registration.showNotification(data.title, {
+//     body: "Notified!",
+//     icon: "http://image.ibb.co/frYOFd/tmlogo.png",
+//   });
+// });
